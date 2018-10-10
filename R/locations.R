4146c41e535fd9e2aeac9b623d03b0d23e44620e
Raman Sangra <sangra.raman@gmail.com>
2018-10-10 16:01:04 +1100

    changes

--- R/locations.R
+++ R/locations.R
@@ -7,8 +7,8 @@
 #' @examples
 #' locations(state ="NSW,suburb="sydney")
 
-locations <- function(state,suburb){
-  m <- mongolite::mongo(collection = Sys.getenv("mcollection"),db =Sys.getenv("mdb"), url = Sys.getenv("mongourl"))
+locations <- function(mongoUrl, mDb, mCollection){
+  m <- mongolite::mongo(collection = mCollection,db =mDb, url = mongoUrl)
   activeLocations <- m$find(
     query = '{"resourceType" : "Location" , "@state" : "current" }',
     fields = '{"name" : true, "status" : true, "address.state" : true , "address.country" : true ,"_id": false}',
