0eb65fe62bc7ef5dcd461bc39e876f5195493fd0
Raman Sangra <sangra.raman@gmail.com>
2018-10-10 15:21:35 +1100

    check env

--- R/locations.R
+++ R/locations.R
@@ -8,5 +8,6 @@
 #' locations(state ="NSW,suburb="sydney")
 
 locations <- function(state,suburb){
-  return(paste("You searched for",state, suburb))
+  Sys.setenv(mongourl="mongourl://xxxx.com")
+  return(paste("You searched for",state, suburb , Sys.getenv("mongourl")))
 }
